96cf44089cd8504dd362fdb508797af01737d301
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2021-10-18 14:59:18 -0300

    Add files via upload

--- Dompratas/index.js
+++ Dompratas/index.js
@@ -34,4 +34,28 @@ $(document).ready(function(){
         var filterValue = $(this).attr("data-filter");
         $grid.isotope({filter: filterValue});
     })
+
+
+    // Contador quantidade de produtos
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    let $input = $(".qty .qty_input");
+
+    // Evento de clique "Contador + quantidade de produtos"
+    $qty_up.click(function(e){
+        if($input.val() >= 1 && $input.val() <= 9){
+           $input.val(function(i, oldval){
+            return ++oldval;
+          });
+        }
+    });
+
+    // Evento de clique "Contador - quantidade de produtos"
+     $qty_down.click(function(e){
+        if($input.val() > 1 && $input.val() <= 10){
+           $input.val(function(i, oldval){
+            return --oldval;
+          });
+        }
+    });
 });
