ac930699cd5c9960b48b88b85bcc99837cb485f2
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2021-11-01 21:51:10 -0300

    Add files via upload

---
+++ site01/Dompratas/cart.php
@@ -0,0 +1,25 @@
+<?php
+// header.php
+include ('header2.php');
+?>
+
+
+<?php
+
+// Carrinho
+include('Template/_cart-template.php');
+// Carrinho
+
+// Mais populares
+include('Template/_mais-populares.php');
+// Mais populares
+
+// Social icons
+include('Template/_social-icons.php');
+// Social icons
+?>
+
+<?php
+// footer.php
+include('footer.php');
+?>
---
+++ site01/Dompratas/database/Cart.php
@@ -0,0 +1,74 @@
+<?php
+
+// php cart class
+class Cart
+{
+   public $db = null;
+
+    public function __construct(DBController $db)
+    {
+        if (!isset($db->con)) return null;
+        $this->db = $db;
+    }
+
+    // inserir no carrinho
+    public function insertIntoCart($params = null, $table = "cart"){
+        if($this->db->con != null){
+            if($params != null){
+                // "insert into cart(user_id) values (0)"
+                // get table columns
+                $columns = implode(',', array_keys($params));
+
+                $values = implode(',', array_values($params));
+
+
+                // create sql query
+                $query_string = sprintf("INSERT INTO %s(%s) VALUES(%s)", $table, $columns, $values);
+
+                // execute query
+                $result = $this->db->con->query($query_string);
+                return $result;
+            }
+        }
+    }
+
+    // to get user_id and item_id and insert into cart table
+    public function addToCart($userid, $itemid){
+        if(isset($userid) && isset($itemid)){
+            $params = array(
+                "user_id" => $userid,
+                "item_id" => $itemid
+            );
+
+            //insert data into cart
+            $result = $this->insertIntoCart($params);
+            if($result){
+
+                // reload page
+                header("Location: " . $_SERVER['PHP_SELF']);  // erro na pagina do carrinho
+            }
+        }
+    }
+
+    // delete cart item using cart item id
+    public function deleteCart($item_id = null, $table = 'cart'){
+        if($item_id != null){
+            $result = $this->db->con->query("DELETE FROM {$table} WHERE item_id={$item_id}");
+            if($result){
+                header("Location:" .$_SERVER['PHP_SELF']);
+            }
+            return $result;
+        }
+    }
+
+    // calculate sub total
+    public function getSum($arr){
+        if(isset($arr)){
+            $sum = 0;
+            foreach ($arr as $item){
+                $sum += floatval($item[0]);
+            }
+            return printf('%.2f',$sum);
+        }
+    }
+}
