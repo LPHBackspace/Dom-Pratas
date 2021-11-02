25e0b9310a16fb3da546dcd29026642160dd791d
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2021-11-02 16:35:55 -0300

    Add files via upload

---
+++ site01/Dompratas/cart.php
@@ -0,0 +1,26 @@
+<?php
+ob_start();
+// header.php
+include ('header2.php');
+?>
+
+
+<?php
+
+// Carrinho
+include('Template/_cart-template.php');
+// Carrinho
+
+// Mais populares
+include('Template/_mais-populares.php');
+// Mais populares
+
+// Social icons
+include('Template/_social-icons.php');
+// Social icons
+?>
+
+<?php
+// footer.php
+include('footer.php');
+?>
