b1e2efa5d4b73c1175c56805d7439fbcf59dd0b5
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2021-11-01 21:52:06 -0300

    Add files via upload

---
+++ site01/Dompratas/Template/_filtrar-itens.php
@@ -0,0 +1,52 @@
+<?php
+
+//request method post
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if(isset($_POST['special_price_submit'])){
+        // call method addToCart
+        $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
+    }
+}
+?>
+
+<!--Filtrar Itens-->
+<section id="filter-items">
+    <div class="container">
+        <h4 class="font-rubik font-size-20">Filtrar itens</h4>
+        <div id="filters" class="button-group text-right font-baloo font-size-16">
+            <button class="btn is-checked" data-filter="*">Todos</button>
+            <button class="btn" data-filter=".Masculino">Masculino</button>
+            <button class="btn" data-filter=".Feminino">Feminino</button>
+        </div>
+
+        <div class="grid">
+            <?php array_map(function($item){ ?>
+            <div class="grid-item border <?php echo $item['item_category'] ?? "Category"; ?>">
+                <div class="item py-2" style="width: 200px;">
+                    <div class="product font-rale">
+                        <a href="<?php printf('%s?item_id=%s', 'product.php', $item['item_id']) ?>"><img src="<?php echo $item['item_image'] ?? 'assents/products/produto1.jpeg'; ?>" alt="Corrente e pulseira masculina 3x1" class="img-fluid"></a>
+                        <div class="text-center">
+                            <h6><?php echo $item['item_name'] ?? 'Unknown' ?></h6>
+                            <div class="rating text-warning font-size-12">
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="far fa-star"></i></span>
+                            </div>
+                            <div class="price py-2">
+                                <span>R$ <?php echo $item['item_price'] ?? '0' ?></span>
+                            </div>
+                            <form method="post">
+                                <input type="hidden" name="item_id" value="<?php echo $item['item_id'] ?? '1'; ?>">
+                                <input type="hidden" name="user_id" value="<?php echo 1; ?>">
+                                <button type="submit" name="special_price_submit" class="btn btn-warning font-size-12">Adicionar ao carrinho</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php }, $product_shuffle) ?>
+        </div>
+</section>
+<!--Filtrar Itens-->
