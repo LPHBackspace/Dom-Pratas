c430107e8e60a053f8abaef3673dfdd3804f6151
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2021-11-02 16:37:03 -0300

    Add files via upload

---
+++ site01/Dompratas/index.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    // carrocel
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots: true,
+        items: 1
+    });
+
+    // Mais vendidos carrocel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        Responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+    // Filtrar itens
+    var $grid = $(".grid").isotope({
+        itemSelector : '.grid-item',
+        layoutMode : 'fitRows'
+    });
+
+    // Filtrar Itens "Botão"
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr("data-filter");
+        $grid.isotope({filter: filterValue});
+    })
+
+
+    // Contador quantidade de produtos
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    // let $input = $(".qty .qty_input");
+
+    // Evento de clique "Contador + quantidade de produtos"
+    $qty_up.click(function(e){
+
+        // mudar o preço do produto "ajax call"
+        $.ajax({url : "template/ajax.php" , type : 'post', data : { itemid : $(this).data("id")} , success : function (result){
+            console.log(result);
+        } })
+
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() >= 1 && $input.val() <= 9){
+            $input.val(function(i, oldval){
+                return ++oldval;
+            });
+        }
+    });
+
+    // Evento de clique "Contador - quantidade de produtos"
+    $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() > 1 && $input.val() <= 10){
+            $input.val(function(i, oldval){
+                return --oldval;
+            });
+        }
+    });
+});
---
+++ site01/Dompratas/product.php
@@ -0,0 +1,25 @@
+<?php
+// header.php
+include ('header.php');
+?>
+
+
+<?php
+
+// Produto
+include('Template/_products.php');
+// Produto
+
+// Mais populares
+include('Template/_mais-populares.php');
+// Mais populares
+
+// Social icons
+include('Template/_social-icons.php');
+// Social icons
+?>
+
+<?php
+// footer.php
+include('footer.php');
+?>
