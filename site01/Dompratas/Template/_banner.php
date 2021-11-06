1e73d455e4f5980c8465ef4b045faf126e7261f3
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2021-11-05 23:18:01 -0300

    Add files via upload

---
+++ site01/Dompratas/Template/_banner.php
@@ -0,0 +1,15 @@
+<!--Carrocel-->
+<section id="banner-area">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <img src="assents/wall1.gif" alt="Banner1">
+        </div>
+        <div class="item">
+            <img src="assents/wall2.gif" alt="Banner2">
+        </div>
+        <div class="item">
+            <img src="assents/wall3.gif" alt="Banner3">
+        </div>
+    </div>
+</section>
+<!--Carrocel-->
---
+++ site01/Dompratas/Template/_social-icons.php
@@ -0,0 +1,15 @@
+<!--Social Icons-->
+<div class="container-fluid padding">
+    <div class="row text-center padding">
+        <div class="col-12">
+            <h2>Redes Sociais</h2>
+        </div>
+        <div class="col-12 social padding">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-tiktok"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+    </div>
+</div>
+<!--Social Icons-->
