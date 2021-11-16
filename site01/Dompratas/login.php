4a3ad71d86e2a86ec6e2c88520d9f6585c7c87e1
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2021-11-15 23:07:29 -0300

    Add files via upload

---
+++ site01/Dompratas/login.php
@@ -0,0 +1,16 @@
+
+<?php
+ob_start();
+// login
+include('Template/loginTemplate/register.php');
+// login
+
+// Social icons
+include('Template/_social-icons.php');
+// Social icons
+?>
+
+<?php
+// footer.php
+include('footer.php');
+?>
