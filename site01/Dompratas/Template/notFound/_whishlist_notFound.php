564cdb0d09bb42a847099d3328c6ab792555bf9f
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2021-11-15 23:06:30 -0300

    Add files via upload

--- site01/Dompratas/Template/notFound/_whishlist_notFound.php
+++ site01/Dompratas/Template/notFound/_whishlist_notFound.php
@@ -12,7 +12,7 @@
 
                 <div class="row border-top py-3 mt-3">
                     <div class="col-sm-12 text-center py-2">
-                        <img src="./assents/empty_cart.png" alt="Carrinho Vazio" class="img-fluid" style="height: 200px;">
+                        <img src="./assents/empty_cart3.png" alt="Carrinho Vazio" class="img-fluid" style="height: 200px;">
                         <p class="font-baloo font-size-16 text-black-50">Nenhum Item Foi Adicionado aos Favoritos</p>
                     </div>
                 </div>
