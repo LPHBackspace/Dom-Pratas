33ca6b5b15fc18cc2a69d593b898ce4f9635a418
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2021-12-18 03:21:23 -0300

    Add files via upload

---
+++ site01/Dompratas/Template/loginTemplate/_register-template.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Meta -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Owl-carousel CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha256-UhQQ4fxEeABh4JrcmAJ1+16id/1dnlOEVCFOxDef9Lw=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha256-kksNxjDRxd/5+jGurZUJd1sdR2v+ClrCl3svESBaJqw=" crossorigin="anonymous" />
+    <!-- font awesome icons -->
+    <script src="https://kit.fontawesome.com/587541520f.js" crossorigin="anonymous"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" href="sass/loginStyle.css">
+
+    <?php
+    // require functions.php
+    require ('functions.php');
+    ?>
+
+
+    <!-- Título e imagem de aba -->
+    <title>Cadastrar - Dom Pratas</title>
+</head>
+<body>
+<!--Começo da Header-->
+<header id="header">
+    <div class="strip d-flex justify-content-between px-4 py-1 bg-light">
+        <p class="font-rale font-size-12 text-black-50 m-0">Dom Pratas Store</p>
+        <div class="font-rale font-size-14">
+            <a href="login.php" class="px-3 border-right border-left text-dark">Login</a>
+            <a href="cart.php" class="px-3 border-right text-dark">Favoritos (O)</a>
+        </div>
+    </div>
+    <!--Nav Bar-->
+    <nav class="navbar navbar-expand-lg navbar-dark color-second-bg">
+        <a class="navbar-brand" href="index.php"><img src="assents/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav m-auto font-rubik">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#footer">Contato</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#top-sale">Produtos<i class="fas fa-chevron-down"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Políticas de Privacidade</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Feedbacks</a>
+                </li>
+            </ul>
+            <form action="#" class="font-size-14 font-rale">
+                <a href="cart.php" class="py-2 rounded-pill color-primary-bg">
+                    <span class="font-size-16 px-2 text-white"><i class="fas fa-shopping-cart"></i></span>
+                    <span class="px-3 py-2 rounded-pill text-dark bg-light"><?php echo count($product->getData('cart')); ?></span>
+                </a>
+            </form>
+        </div>
+    </nav>
+</header>
+<!--Main Site-->
+<main id="main-site">
+<div class="login-header">
+    <h2>Registrar-se</h2>
+</div>
+
+    <form class="login_box" method="post" action="database/User.php">
+        <div class="input-group">
+            <label>Nome de Usuário</label>
+            <input type="text" name="username">
+        </div>
+        <div class="input-group">
+            <label>E-mail</label>
+            <input type="email" name="email">
+        </div>
+        <div class="input-group">
+            <label>Telefone</label>
+            <input type="text" name="tel">
+        </div>
+        <div class="input-group">
+            <label>Data de Nascimento</label>
+            <input type="date" name="birth_date">
+        </div>
+        <div class="input-group">
+            <label>Endereço</label>
+            <input type="text" name="adress">
+        </div>
+        <div class="input-group">
+            <label>Senha</label>
+            <input type="password" name="password">
+        </div>
+        <div class="input-group">
+            <button type="submit" class="btn">Criar Conta</button>
+        </div>
+        <p>Já possui conta? <a href="login.php">Entrar</a></p>
+    </form>
